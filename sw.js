88db9121d036246acd5a4f833a6d9c80ba992f0a
KAKEGURUI00 <69821672+KAKEGURUI00@users.noreply.github.com>
2026-07-17 20:54:48 +0300

    NPC sayfasına etkileşimli 3D modeller ekle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kage-v3.1';
+const CACHE='kage-v3.2-npc3d';
 const CORE=['./','index.html','sistemler.html','dunya.html','ekonomi.html','meslekler.html','irklar.html','kasaba.html','olaylar.html','npcler.html','rehber.html','gelistirme.html','sunucu.html','galeri.html','hakkinda.html','404.html','assets/style.css','assets/pages.css','assets/details.css','assets/config.js','assets/app.js','assets/pack-icon.png','assets/kage-world-visual.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
